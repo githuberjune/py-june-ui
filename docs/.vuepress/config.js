a7fc3d9fa4d336b06de0046983155d076194d357
June <13430308612@163.com>
2023-03-18 12:12:27 +0800

    增加笔记描述

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,21 @@
 module.exports = {
+  title: "Py-June-UI",
+  base: "/py-june-ui/",
   themeConfig: {
+    nav: [
+      {
+        text: "Home",
+        link: "/",
+      },
+      {
+        text: "Github",
+        link: "https://github.com/githuberjune/py-june-ui",
+      },
+      {
+        text: "Npm",
+        link: "https://www.npmjs.com/package/py-june-ui",
+      },
+    ],
     sidebar: ["/", "/componentDocs/card"],
   },
 };
