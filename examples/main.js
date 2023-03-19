1eb1332b496307d4e496790b676609571a20d21e
June <13430308612@163.com>
2023-03-19 17:33:15 +0800

    june:修改webpack版本为4，调整其他所有配套包版本，使得build,docs:dev,serve都能正常运行

--- examples/main.js
+++ examples/main.js
@@ -2,8 +2,8 @@ import Vue from "vue";
 import App from "./App.vue";
 
 //todo 引入公共样式
-// import "../components/css/demo.scss";
-// import "../components/css/card.scss";
+import "../components/css/demo.scss";
+import "../components/css/card.scss";
 
 //! 组件库是插件形式，不再使用引入整个vue来做引入
 // import Demo from "../components/lib/src/demo/main.vue";
@@ -15,13 +15,13 @@ import App from "./App.vue";
 // Vue.use(Card);
 
 //todo2 组件库整体引入
-// import API from "../components/lib/index";
-// API.install(Vue);
+import API from "../components/lib/index";
+API.install(Vue);
 
 //todo 从npm引入
-import "py-june-ui/dist/css/index.css";
-import MUI from "py-june-ui";
-Vue.use(MUI);
+// import "py-june-ui/dist/css/index.css";
+// import MUI from "py-june-ui";
+// Vue.use(MUI);
 
 Vue.config.productionTip = false;
 
